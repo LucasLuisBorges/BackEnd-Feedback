18e73c3f531e7cb5b230accc27a38c7c22f47167
Lucas Luis Borges <lucasluisborges@outlook.com>
2022-05-05 11:55:34 -0300

    add cors lib

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
-import express from 'express'
+import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 
 // GET = Buscar informação
@@ -9,6 +10,7 @@ import { routes } from './routes';
 
 const app = express();
 
+app.use(cors())
 app.use(express.json())
 app.use(routes)
 
