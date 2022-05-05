9af5758ef8ca382bd64c6df65361fe451f5049d1
Lucas Luis Borges <lucasluisborges@outlook.com>
2022-05-05 11:48:53 -0300

    complete test submet feedback use case screen

--- src/use-cases/submit-feedback-use-case.ts
+++ src/use-cases/submit-feedback-use-case.ts
@@ -16,6 +16,18 @@ export class SubmitFeedbackUseCase {
   async execute(request: SubmitFeedbackUseCaseRequest) {
     const { type, comment, screenshot } = request;
 
+    if (screenshot && !screenshot.startsWith('data:image/png;base64')) {
+      throw new Error('Invalid screenshot format.')
+    }
+
+    if (!type) {
+      throw new Error('Type is required')
+    }
+
+    if (!comment) {
+      throw new Error('comment is required')
+    }
+
     await this.feedbacksRepository.create({
       type,
       comment,
